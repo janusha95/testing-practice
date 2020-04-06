baac2947e6dd0d5a2ead38ef5592aec75734a010
janusha95 <60120739+janusha95@users.noreply.github.com>
2020-04-06 13:39:45 +0530

    added test case 5 for quotient with no number

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -3,6 +3,7 @@
 // ==================================================
 function quotient(num1, num2) {
   if (num2 === 0) return 'ERROR';
+  if (num1 === undefined) num1=0;
   if (num2 === undefined) num2=1;
   return num1 / num2;
 }
@@ -43,6 +44,8 @@ try {
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = quotient();
+  if (result !== 0) throw new Error('Expected quotient() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
